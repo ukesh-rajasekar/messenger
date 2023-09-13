ab46f9bcaea71f906749ca94437af1feda695382
ukesh-rajasekar <54039014+ukesh-rajasekar@users.noreply.github.com>
2023-09-13 20:03:54 +1000

    feat: reStructuring update

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-import Button from '../../../components/ui/Button'
+import Button from '../../components/ui/Button'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/auth'
 
