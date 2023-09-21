851b96eff88a8b191fc592f3caa46ad339559418
ukesh-rajasekar <54039014+ukesh-rajasekar@users.noreply.github.com>
2023-09-21 18:33:58 +1000

    fix: loading state added

--- src/components/AddFriendButton.tsx
+++ src/components/AddFriendButton.tsx
@@ -8,12 +8,13 @@ import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { emailValidator } from '@/lib/validators/email'
 
-interface AddFriendButtonProps {}
+interface AddFriendButtonProps { }
 
 type FormData = z.infer<typeof emailValidator>
 
-const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
+const AddFriendButton: FC<AddFriendButtonProps> = ({ }) => {
   const [showSuccessState, setShowSuccessState] = useState<boolean>(false)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
 
   const {
     register,
@@ -25,6 +26,7 @@ const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
   })
 
   const addFriend = async (email: string) => {
+    setIsLoading(true)
     try {
       const validatedEmail = emailValidator.parse({ email })
 
@@ -45,6 +47,8 @@ const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
       }
 
       setError('email', { message: 'Something went wrong.' })
+    } finally {
+      setIsLoading(false)
     }
   }
 
@@ -67,7 +71,7 @@ const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
           className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
           placeholder='you@example.com'
         />
-        <Button>Add</Button>
+        <Button isLoading={isLoading}>Add</Button>
       </div>
       <p className='mt-1 text-sm text-red-600'>{errors.email?.message}</p>
       {showSuccessState ? (
