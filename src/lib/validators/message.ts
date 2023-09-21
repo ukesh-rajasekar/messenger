5de5d9ed78487efb6796011f1165349d5838662b
ukesh-rajasekar <54039014+ukesh-rajasekar@users.noreply.github.com>
2023-09-21 18:35:26 +1000

    feat: messageValidators added, cleanups

--- src/lib/validators/message.ts
+++ src/lib/validators/message.ts
@@ -1,13 +1,13 @@
-import {z} from 'zod'
+import { z } from "zod";
 
 export const messageValidator = z.object({
-    id:z.string(),
-    senderId: z.string(),
-    receiverId: z.string(),
-    text: z.string(),
-    timestamp: z.number(),
-})
+  id: z.string(),
+  senderId: z.string(),
+  receiverId: z.string(),
+  text: z.string().max(300),
+  timestamp: z.number(),
+});
 
-export const messageArrayValidator = z.array(messageValidator)
+export const messageArrayValidator = z.array(messageValidator);
 
-export type Message = z.infer<typeof messageValidator>
+export type Message = z.infer<typeof messageValidator>;
