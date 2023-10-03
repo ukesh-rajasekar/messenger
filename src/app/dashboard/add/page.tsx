5b6c5f0d44a5300704d56cefb93451d51186bec7
Ukesh Rajasekar <z5288900@ad.unsw.edu.au>
2023-10-03 21:57:46 +1100

    feat: middleware fix

--- src/app/dashboard/add/page.tsx
+++ src/app/dashboard/add/page.tsx
@@ -1,17 +1,18 @@
-import AddFriendButton from '@/components/AddFriendButton'
-import { FC } from 'react'
-import Skeleton from 'react-loading-skeleton'
+import AddFriendButton from '@/components/AddFriendButton';
+import { FC } from 'react';
+import Skeleton from 'react-loading-skeleton';
 
-interface AddProps {
+interface AddProps {}
 
-}
+const Add: FC<AddProps> = ({}) => {
+   return (
+      <main className='pt-8'>
+         <h1 className='font-bold text-5xl mb-8 ml-2 text-black'>
+            Add a friend
+         </h1>
+         <AddFriendButton />
+      </main>
+   );
+};
 
-const Add: FC<AddProps> = ({ }) => {
-
-    return (<main className='pt-8'>
-        <h1 className='font-bold text-5xl mb-8 ml-2'>Add a friend</h1>
-        <AddFriendButton />
-    </main>)
-}
-
-export default Add
+export default Add;
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,20 +5,25 @@ import { NextResponse } from 'next/server';
 export default withAuth(
    async function middleware(req) {
       const pathname = req.nextUrl.pathname;
+
+      // Manage route protection
       const isAuth = await getToken({ req });
       const isLoginPage = pathname.startsWith('/login');
 
       const sensitiveRoutes = ['/dashboard'];
-
-      const isAccessingSensitiveRoutes = sensitiveRoutes.some((route) =>
+      const isAccessingSensitiveRoute = sensitiveRoutes.some((route) =>
          pathname.startsWith(route)
       );
 
-      if (isLoginPage && isAuth) {
-         return NextResponse.redirect(new URL('/dashboard', req.url));
+      if (isLoginPage) {
+         if (isAuth) {
+            return NextResponse.redirect(new URL('/dashboard', req.url));
+         }
+
+         return NextResponse.next();
       }
 
-      if (isAccessingSensitiveRoutes && !isAuth) {
+      if (!isAuth && isAccessingSensitiveRoute) {
          return NextResponse.redirect(new URL('/login', req.url));
       }
 
